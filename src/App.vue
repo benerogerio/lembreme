2554f9ffe3c13287e2c49cc1c34fa65fdb316213
Rogerio Bene <bene.rogerio@hotmail.com>
2019-07-08 00:45:28 -0300

    testando com todo list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Testando msg" variavel="testando atributos"/>
-  </div>
+	<div id="app">
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
+		<h1>Minha Tarefas!</h1>
+		<TodoList/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	components: {
+		TodoList
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	max-width: 400px;
+	margin: 0 auto;
+	line-height: 1.4;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: blue;
+}
+
+h1 {
+	text-align: center;
 }
 </style>
