40943471a28314808b7bc2a4702de1d63639c153
Rogerio Bene <bene.rogerio@hotmail.com>
2022-08-21 12:10:17 -0300

    corrige vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   publicPath: '',
+
   pluginOptions: {
     cordovaPath: 'src-cordova'
   }
-})
+}
